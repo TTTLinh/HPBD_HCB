1250a2dfac2da256b849b35eca7bc14ef6fbc437
TTTLinh <bui15062000@gmail.com>
2024-06-16 11:33:59 +0700

    love

---
+++ config.js
@@ -0,0 +1,51 @@
+var config = {
+   
+    texts: [
+        "Xin chào anh yêu của em",      
+        "Tui là em bé của anh nè <3", 
+        "Đây là năm đầu tiên em đón sinh nhật với anh", 
+        "Ngày sinh nhật của anh, em có vài điều muốn nói",
+        "Đầu tiên em chúc mừng anh đã chính thức già đi 1 tuổi",
+        "và hiện tại anh đã có được em",
+        "Gửi ngàn lời chúc tới anh",
+        "Chúc anh ngày đẹp trai, xinh xắn hơn",
+        "Chúc anh luôn có nhiều nhiều sức khỏe, mạnh mẽ và vui vẻ",
+        "Chúc anh lúc nào cũng may mắn và thành công trong cuộc sống",
+        "Chúc anh nhanh nhanh giàu để có thể nuôi em",
+        "Chúc anh có thể yêu em nhiều nhiều hơn",
+        "Chúc anh có thể cầm lấy tay em lâu hơn",
+        "Và chúc anh tất cả <3",
+        "EM YÊU ANH HOÀNG CÔNG BẮC",
+        "Em bé của anh, tling hehehe",
+    ],
+   
+    imgs: {
+        "Xin chào anh yêu của em": "./imgs/hello.gif",
+        "Tui là em bé của anh nè <3": "./imgs/hì.jpg",
+        "Đây là năm đầu tiên em đón sinh nhật với anh": "./imgs/hpbd.gif",
+        "Đầu tiên em chúc mừng anh đã chính thức già đi 1 tuổi": "./imgs/haha.gif",
+        "và hiện tại anh đã có được em": "./imgs/anh.jpg",
+        "Gửi ngàn lời chúc tới anh": "./imgs/love.gif",
+        "Chúc anh ngày đẹp trai, xinh xắn hơn": "./imgs/anhhi.jpg",
+        "Chúc anh luôn có nhiều nhiều sức khỏe, mạnh mẽ và vui vẻ": "./imgs/khoe.jpg",
+        "Chúc anh lúc nào cũng may mắn và thành công trong cuộc sống": "./imgs/mayman.jpg",
+        "Chúc anh nhanh nhanh giàu để có thể nuôi em": "./imgs/giau.jpg",
+        "Chúc anh có thể yêu em nhiều nhiều hơn": "./imgs/sinhnhat.gif",
+        "Chúc anh có thể cầm lấy tay em lâu hơn": "./imgs/tay.jpg",
+        "Và chúc anh tất cả <3": "./imgs/a.gif",
+        "EM YÊU ANH HOÀNG CÔNG BẮC": "./imgs/yeu.gif",
+        "Em bé của anh, tling hehehe": "./imgs/aa.jpg",
+    },
+  
+    
+    // desc: {
+    //     turn_on: "Anh đã sẵn ",
+    //     play: "音乐",
+    //     bannar_coming: "颜色",
+    //     balloons_flying: "好像少点东西",
+    //     cake_fadein: "蛋糕？",
+    //     light_candle: "蜡烛？",
+    //     wish_message: "生日快乐",
+    //     story: "A MESSAGE FOR YOU",
+    // }
+};
